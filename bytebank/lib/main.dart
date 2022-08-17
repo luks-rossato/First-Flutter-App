295abe57d8b493d78c29e0aee996fba40400aca7
Lucas Rodrigues Rossato <89791115+luks-rossato@users.noreply.github.com>
2022-08-16 21:19:31 -0300

    update in main

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -27,54 +27,65 @@ class FormularioTransferencia extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _controladorCampoNumeroConta,
-                style: TextStyle(),
-                decoration: InputDecoration(
-                  hintText: '0000-0',
-                  helperText: 'Digite aqui o numero da sua conta',
-                  labelText: 'Numero da conta',
-                  fillColor: Colors.deepPurple,
-                ),
-                keyboardType: TextInputType.number,
-                cursorColor: Colors.deepPurple,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _controladorCampoValor,
-                decoration: InputDecoration(
-                    hintText: '0.00',
-                    labelText: 'Valor da Transferência',
-                    icon: Icon(
-                      Icons.monetization_on,
-                      color: Colors.deepPurple,
-                    )),
-                keyboardType: TextInputType.number,
-                cursorColor: Colors.deepPurple,
-              ),
-            ),
+            Editor(
+                controlador: _controladorCampoNumeroConta,
+                rotulo: 'Numero da Conta',
+                dica: '000'),
+            Editor(
+                controlador: _controladorCampoValor,
+                rotulo: 'Valor da Transferência',
+                dica: '0.00',
+                icone: Icons.monetization_on),
             ElevatedButton(
                 onPressed: () {
-                  print('Clicou no Confirmar');
-                  final int numeroconta =
-                      int.parse(_controladorCampoNumeroConta.text);
-                  final double valor =
-                      double.parse(_controladorCampoValor.text);
-                  if (numeroconta != Null && valor != Null) {
-                    final transferenciaCriada =
-                        Transferencia(valor, numeroconta);
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('Transferência de $valor reais '
-                            'para a conta $numeroconta!')));
-                  }
+                  _criaTransferencia(context);
                 },
                 child: Text('Confirmar'))
           ],
         ));
+  }
+
+  void _criaTransferencia(BuildContext context) {
+    final int numeroconta = int.parse(_controladorCampoNumeroConta.text);
+    final double valor = double.parse(_controladorCampoValor.text);
+    if (numeroconta != Null && valor != Null) {
+      final transferenciaCriada = Transferencia(valor, numeroconta);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Transferência de $valor reais '
+              'para a conta $numeroconta!')));
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData? icone;
+
+  Editor(
+      {required this.controlador,
+      required this.rotulo,
+      required this.dica,
+      this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          hintText: dica,
+          labelText: rotulo,
+          fillColor: Colors.deepPurple,
+        ),
+        keyboardType: TextInputType.number,
+        cursorColor: Colors.deepPurple,
+      ),
+    );
   }
 }
 
